5888fb421d033edfdb981fbd241960b716febf88
emptycan <richardtrapanese@gmail.com>
2018-05-15 17:15:58 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
 ## code your solution here. 
+class  Cat
+  attr_accessor :name #kool getter/setter attribute accessor
+
+  def meow
+    puts "meow!"
+  end
+
+
+
+
+
+
+end
--- spec/meowing_cat_spec.rb
+++ spec/meowing_cat_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/meowing_cat'
 
 describe "Cat" do
   let(:maru) {Cat.new}  # Look up what let does in RSpec
-  # https://www.relishapp.com/rspec/rspec-core/v/2-6/docs/helper-methods/let-and-let
+  #https://www.relishapp.com/rspec/rspec-core/v/2-6/docs/helper-methods/let-and-let
 
   it 'instantiates a new cat' do
     expect(maru).to be_a(Cat)
